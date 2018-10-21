e1f93afe23c05d5dcc566e5673fd4c8bafa80aa4
Boris <into.the.deepest.space@gmail.com>
2018-10-21 17:31:08 -0400

    add boost call

--- test_cross_platform/main.cpp
+++ test_cross_platform/main.cpp
@@ -1,6 +1,9 @@
+#include <boost/multiprecision/cpp_int.hpp>
 #include "my_class.hpp"
 
 int main() {
+	boost::multiprecision::cpp_int c("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+	std::cout << c << std::endl;
 	MyClass my_instance(42);
 	my_instance.my_function();
 }
